1e3ed41d5683c803579eba537000e357a255f387
LB <brault.13.ludovic@gmail.com>
2021-10-22 16:31:57 +0200

    Dashboard interface

---
+++ src/Form/UserHierarchyType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserHierarchyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('parent', EntityType::class, [
+                'class' => User::class,
+                'expanded' => false,
+                'multiple' => false,
+                'choice_label' => 'lastname',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')                 
+                    ->orderBy('u.lastname', 'ASC');
+                }
+            ])
+            ->add('register', SubmitType::class, [
+                'attr' => [
+                    'class' => 'mt-2 btn-danger'
+                ]
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
